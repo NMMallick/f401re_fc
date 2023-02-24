26ee75dd4d5fa15f218a9be970df5cd6d776b5c2
NMMallick <nmm109@pitt.edu>
2023-02-24 11:40:33 -0500

    ...

--- Core/Src/Offboard.c
+++ Core/Src/Offboard.c
@@ -27,6 +27,24 @@ void check_buffer()
     for (int i = 0; i < TX_BUF_SIZE; i++)
         offb_dtype->tx_buf[i] = offb_dtype->rx_buf[i];
 
+    float axes[4];
+    axes[0] = *(float *)(&offb_dtype->tx_buf[0]);
+    axes[1] = *(float *)(&offb_dtype->tx_buf[4]);
+    axes[2] = *(float *)(&offb_dtype->tx_buf[8]);
+    axes[3] = *(float *)(&offb_dtype->tx_buf[12]);
+
+    axes[0] /= axes[0];
+    axes[1] = 0.5;
+    axes[2] /= axes[2];
+    axes[3] = 0.6;
+
+    uint8_t *ptr = (uint8_t*)axes;
+
+    for (int i = 0; i < 16; i++)
+    {
+        offb_dtype->tx_buf[i] = *(ptr + i);
+    }
+
     HAL_UART_Transmit_DMA(offb_dtype->huart, offb_dtype->tx_buf, TX_BUF_SIZE);
     // for (uint32_t i = 0; i < RX_BUF_SIZE; i ++)
     //     offb_dtype->rx_buf[i] = 0x00;
